04c3ad698f5d24feee3a5e798b1127f03d15d8bc
Juan Felipe Rangel <juan.rangel-r@mail.escuelaing.edu.co>
2026-07-14 12:27:58 -0500

    Fix import statement for Socket.io

--- src/Infrastructure/Outbound/WebSocket/ChatWebSocket.ts
+++ src/Infrastructure/Outbound/WebSocket/ChatWebSocket.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CHAT_PORTS } from 'src/Application/Ports/Out/ChatTokens';
 import { ChatWs } from 'src/Application/Ports/Out/ChatWs';
 import { ChatSocketController } from 'src/Infrastructure/Inbound/Chat/ChatSocketController';
-import { Server } from 'Socket.io';
+import { Server } from 'socket.io';
 import { Message } from 'src/Domain/Model/Message';
 
 @Injectable()
